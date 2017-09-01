a9d64db3f6109f2f46835ffaeaa862796264856e
Moysés Oliveira <contato@moysesoliveira.com.br>
2017-09-01 11:17:34 -0300

    table Render update

--- lib/UI/JQuery/DataTable/TableRender.php
+++ lib/UI/JQuery/DataTable/TableRender.php
@@ -2,6 +2,7 @@
 
 namespace Spell\UI\JQuery\DataTable;
 
+use Spell\UI\HTML\Tag;
 /**
  * Render Datatable HTML
  *
@@ -10,47 +11,87 @@ namespace Spell\UI\JQuery\DataTable;
 class TableRender
 {
 
+    /**
+     *
+     * @var TableConfig 
+     */
     private $dtTbConfig = null;
+    
+    /**
+     *
+     * @var Tag
+     */
+    private $container = null;
+    
+    /**
+     *
+     * @var string 
+     */
     private $dtTbId = null;
+    
+    /**
+     *
+     * @var bool
+     */
+    private $autoInvoke = true;
 
     public function __construct(TableConfig $dtTbConfig, $dtTbId = 'app-data-table-config')
     {
         $this->dtTbConfig = $dtTbConfig;
         $this->dtTbId = $dtTbId;
+        $this->container = Tag::div();
+    }
+    
+    public function setAutoInvoke(bool $bool) {
+        $this->autoInvoke = $bool;
+    }
+    
+    public function getAutoInvoke():bool {
+        return $this->autoInvoke;
+    }
+    
+    public function getContainer(): Tag {
+        return $this->container;
     }
 
     public function renderJson()
     {
         $json = json_encode($this->dtTbConfig->getConfig(), JSON_PRETTY_PRINT);
-        return PHP_EOL . '<script type="application/json" id="' . $this->dtTbId . '">' . PHP_EOL . $json . PHP_EOL . '</script>';
+        return Tag::mk('script')
+            ->setAttr('type', 'application/json')
+            ->setAttr('id', $this->dtTbId)
+            ->setContent(PHP_EOL . $json . PHP_EOL)
+            ->render();
     }
 
     public function renderHtmlTable()
     {
-        $th = '';
+        
+        $body = $this->getContainer();
+        $body->addClass('form-group');
+        $body->setAttr('app-config', "#{$this->dtTbId}");
+        
+        if($this->getAutoInvoke())
+            $body->setAttr('data-spell_data_table', '1');
+        
+        $body->appendChild(Tag::div('navbar-header col-xs-12'));
+        $body->appendChild(Tag::div('clearfix')->setContent('&nbsp;'));
+        
         $sortDefault = $this->dtTbConfig->getSortDefault();
         $orientation = $this->dtTbConfig->getSortDesc() ? 'desc' : 'asc';
 
+        $th = new \ArrayObject();
         foreach ($this->dtTbConfig->getColumns() as $c)
-            $th .= PHP_EOL . str_repeat("\t", 4) . '<th>' . $c->getLabel() . '</th>';
-
-        return <<<HTML
-		
-<div class="form-group" app-config="#{$this->dtTbId}" data-spell_data_table="true">
-	<div class="navbar-header col-xs-12">
-	</div>
-	<div style="clear: both;"></div><br/>
-	<table class="data table table-bordered table-hover" data-order='[[$sortDefault, "$orientation" ]]'>
-		<thead>
-			<tr class="stylegrids data-table-header">$th
-			</tr>
-		</thead>
-		<tbody>
-		</tbody>
-	</table>
-</div>
-		
-HTML;
+            $th->append(Tag::mk('th')->setContent($c->getLabel()));
+        
+        $headerRow = Tag::mk('tr', 'stylegrids data-table-header')->setChilds($th->getArrayCopy());
+        
+        $table = Tag::mk('table', 'data table table-bordered table-hover')
+            ->setAttr('data-order', sprintf('[[%s, "%s" ]]', $sortDefault, $orientation));
+        
+        $table->appendChild(Tag::mk('thead')->appendChild($headerRow));
+        $table->appendChild(Tag::mk('tbody'));
+        return $body->appendChild($table)->render();
     }
 
     public function render()
