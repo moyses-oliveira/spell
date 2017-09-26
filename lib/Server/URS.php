99d87dacd43496af9b14e0e7f8c3dd49f8c556df
Moysés Oliveira <contato@moysesoliveira.com.br>
2017-09-25 21:33:55 -0300

    fix urs on server

--- lib/Server/URS.php
+++ lib/Server/URS.php
@@ -254,6 +254,28 @@ class URS {
     }
 
     /**
+     * 
+     * @param string $str
+     * @param array $replace
+     * @param string $delimiter
+     * @return string
+     */
+    public function normalize(string $str, array $replace = [], string $delimiter = '-'): string
+    {
+        setlocale(LC_ALL, 'en_US.UTF8');
+        if(!empty($replace)) {
+            $str = str_replace((array) $replace, ' ', $str);
+        }
+
+        $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
+        $clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+        $clean = strtolower(trim($clean, '-'));
+        $clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
+
+        return $clean;
+    }
+
+    /**
      * Get code from builded friendly URL 
      * 
      * @param string $url
